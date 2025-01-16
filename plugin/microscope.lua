fe217c1757cb68052e4e6091a7f520529fcd15db
itsamayo <ashleymichaelsanders@gmail.com>
2025-01-16 09:28:31 -0800

    fix: module ref

--- plugin/microscope.lua
+++ plugin/microscope.lua
@@ -1,2 +1,2 @@
-vim.api.nvim_create_user_command("MSFold", require("microscope.module").fold_excep_highlighted, {})
-vim.api.nvim_create_user_command("MSUnfold", require("microscope.module").unfold_all, {})
+vim.api.nvim_create_user_command("MSFold", require("microscope.module").fold_except_highlighted(), {})
+vim.api.nvim_create_user_command("MSUnfold", require("microscope.module").unfold_all(), {})
